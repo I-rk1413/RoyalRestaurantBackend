78c0c862130c46d96ae4d0ad953f022d9ce5890e
Rahul <rahulkr1413@gmail.com>
2020-07-27 09:47:47 +0530

    Added the mongo server

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
 module.exports={
 	'secretkey':'12345-67890-09876-54321',
-	'mongoUrl':'mongodb://localhost:27017/conFusion',
-	'facebook':{
-		clientId:'287334345866243',
-		clientSecret:'3982090b163ca80620ee61fa2525310f'
-}
+	'mongoUrl':'mongodb+srv://royalrestaurant:royal@clusterroyal.ujzk8.mongodb.net/<dbname>?retryWrites=true&w=majority',
+	
 }
