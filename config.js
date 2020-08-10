7fd07d8eb34c1759566ef11f6b318e8854a9fb0a
Rahul <rahulkr1413@gmail.com>
2020-08-10 17:14:20 +0530

    Adde mongodb atlas

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports={
 	'secretkey':'12345-67890-09876-54321',
-	'mongoUrl':'mongodb://localhost:27017/conFusion',
-	// 'mongoUrl':'mongodb+srv://royalrestaurant:royal@clusterroyal.ujzk8.mongodb.net/<dbname>?retryWrites=true&w=majority',
+	// 'mongoUrl':'mongodb://localhost:27017/conFusion',
+	'mongoUrl':'mongodb+srv://royalrestaurant:royal@clusterroyal.ujzk8.mongodb.net/conFusion?retryWrites=true&w=majority',
 	// AIzaSyCPtlWyic07P7MdTf5Wm16UgpXC7y3XXbw
 		'facebook':{
 		clientId:'287334345866243',
